8d9941745229bc05e677dc2396f841fc5534478a
Hussain Ali <iamhussainali2@gmail.com>
2024-01-21 08:49:15 +0530

    first commit

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos:[{id:1, text:"Hello World"}]
+}
+
+
+
+export const todoSlice = createSlice({
+    name:"todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id:nanoid(),
+                text: action.payload
+            };
+            state.todos.push(todo)
+
+        },
+        removeTodo: (state, action) => {}
+    }
+})
